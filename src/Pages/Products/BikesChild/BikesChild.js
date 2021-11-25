03e1a3a21541bff0a49af1d6d1e358aa705aa7b2
kawsarm104 <kawsarm104@gmail.com>
2021-11-26 03:53:24 +0600

    modify card

--- src/Pages/Products/BikesChild/BikesChild.js
+++ src/Pages/Products/BikesChild/BikesChild.js
@@ -2,29 +2,32 @@ import React from "react";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import { Link } from "react-router-dom";
+import "./BIkeChild.css"
 
 const BikesChild = (props) => {
   const { _id, name, price, details, photoUrl } = props.service;
   return (
-    <div className=" col-sm-12 col-md-6 col-lg-3 g-1 my-2 ">
+    <div className=" col-sm-12 col-md-6 col-lg-3 g-1 my-2 bike-card">
       <Card className="h-100 m-auto border-1">
         <Card.Img variant="top" className="img-fluid" src={photoUrl} />
-        <Card.Body className="text-center">
-          <Card.Title>{name}</Card.Title>
-          <Card.Text className="fw-bold">
-            Price: <span className="text-warning">${price}</span>
-          </Card.Text>
-          {/* <Card.Text>
+        <div className="overlay">
+          <Card.Body className="text-center ">
+            <Card.Title>{name}</Card.Title>
+            <Card.Text className="fw-bold">
+              Price: <span className="text-warning">${price}</span>
+            </Card.Text>
+            {/* <Card.Text>
             {details.split(" ").slice(0, 10).toString().replace(/,/g, " ")}
           </Card.Text> */}
-        </Card.Body>
-        <Card.Footer className="d-flex justify-content-end border-0 shadow-none bg-white">
-          <Link to={`/purchase/${_id}`}>
-            <Button variant="secondary " className="p-2  rounded">
-              View Details
-            </Button>{" "}
-          </Link>
-        </Card.Footer>
+          </Card.Body>
+          <Card.Footer className="d-flex justify-content-end border-0 shadow-none">
+            <Link to={`/purchase/${_id}`}>
+              <Button variant="secondary " className="p-2  rounded details-button">
+                View Details
+              </Button>{" "}
+            </Link>
+          </Card.Footer>
+        </div>
       </Card>
     </div>
   );
